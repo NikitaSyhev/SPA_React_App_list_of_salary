ba5d2cf8bdc2ae21b4ebed91d2162f195ae09907
Nikita Sychev <nikitasychev@inbox.ru>
2024-12-11 21:02:59 +0500

    add controlled components

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,21 +1,53 @@
+import { Component } from 'react';
+
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавить нового сотрудника: </h3>
-            <form 
-            className="add-form d-flex">
-            <input type="text" 
-                className="form-control new-post-label" 
-                placeholder="Как его зовут?"/>
-            <input type="number" 
-                placeholder="З.П. в рублях"/>
-                className="form-control new-post-label" 
-            <button className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    );
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            name:'',
+            salary: '',
+        }
+    }
+
+    onValueVhange=(e)=> {
+        this.setState(
+            {
+                [e.target.name] :e.target.value,
+            }
+        )
+    }
+
+
+    render(){
+        const {name, salary} = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавить нового сотрудника: </h3>
+                <form 
+                className="add-form d-flex">
+                <input type="text" 
+                    className="form-control new-post-label" 
+                    placeholder="Как его зовут?"
+                    name="name"
+                    // атрибут value создаем управляемый компонент react
+                    value={name}
+                    onChange={this.onValueVhange}/>
+                <input type="number" 
+                    placeholder="З.П. в рублях"
+                    name="salary"
+                    // атрибут value создаем управляемый компонент react
+                    value={salary}
+                    onChange={this.onValueVhange}/>
+                    className="form-control new-post-label" 
+                <button className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        );
+    }
+  
 }
 
 export default EmployeesAddForm;
